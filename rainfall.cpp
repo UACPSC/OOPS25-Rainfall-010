27deb746c6b29f63b76762512b29b098f5db8ce9
Michael L. Collard <collard@uakron.edu>
2025-01-21 10:27:54 -0500

    Move local variable declaration of m to where first used

--- rainfall.cpp
+++ rainfall.cpp
@@ -13,7 +13,6 @@ int main() {
 
     // input hourly rainfall data
     std::vector<double> rainfall;
-    double m;
     double n;
     while (std::cin >> n) {
         rainfall.push_back(n);
@@ -25,7 +24,7 @@ int main() {
 
     // calculate the average and heaviest rainfall
     double t = rainfall[0];
-    m = rainfall[0];
+    double m = rainfall[0];
     for (std::vector<double>::size_type i = 1; i < rainfall.size(); ++i) {
         t += rainfall[i];
         if (rainfall[i] > m)
